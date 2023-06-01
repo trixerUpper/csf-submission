5f1ae510e2fc5ec1f84fadf0e0bd39d831f12bde
Trix <trrrixlim@gmail.com>
2023-06-01 11:38:56 +0800

    task2

--- pizza-storefront/src/app/app.module.ts
+++ pizza-storefront/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main.component';
+import { OrdersComponent } from './components/orders.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:"", component: MainComponent},
+  {path:"orders/:email", component: OrdersComponent},
+  {path:"**", redirectTo: "/", pathMatch: "full"}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
+    OrdersComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
 
   providers: [ ],
--- pizza-storefront/src/app/components/main.component.ts
+++ pizza-storefront/src/app/components/main.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 const SIZES: string[] = [
   "Personal - 6 inches",
@@ -17,11 +18,36 @@ const PIZZA_TOPPINGS: string[] = [
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
-
+export class MainComponent implements OnInit{
+  
+  myForm: FormGroup | null = null;
+  toppings: FormGroup | null = null;
   pizzaSize = SIZES[0]
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.toppings = this.formBuilder.group({
+      chicken: this.formBuilder.control(false),
+      seafood: this.formBuilder.control(false),
+      beef: this.formBuilder.control(false),
+      vegetables: this.formBuilder.control(false),
+      cheese: this.formBuilder.control(false),
+      arugula: this.formBuilder.control(false),
+      pineapple: this.formBuilder.control(false),
+    });
+
+    this.myForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      size: ['', Validators.required],
+      base: ['', Validators.required],
+      sauce: ['', Validators.required],
+      toppings: this.toppings,
+      comments: ['']
+    });
+  }
 
   updateSize(size: string) {
     this.pizzaSize = SIZES[parseInt(size)]
